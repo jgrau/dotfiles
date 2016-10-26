4bf4d1aaa5caabcfbc8b8fa2eff037bb09388a05
jgrau <jonas.grau@gmail.com>
2016-10-26 17:29:47 +0200

    Changed terminal

--- slate.js
+++ slate.js
@@ -23,8 +23,9 @@ slate.configAll({
 });
 
 // Focus apps
-slate.bind("-:ctrl", $.focus('Google Chrome'));
-slate.bind("=:ctrl", $.focus('iTerm'));
+slate.bind("=:ctrl", $.focus('Google Chrome'));
+// slate.bind("-:ctrl", $.focus('iTerm2'));
+slate.bind("-:ctrl", $.focus('Hyper'));
 
 // bindings
 slate.bindAll({

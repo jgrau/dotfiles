a3683143b9f0e91dc9faef7955caf07fa70c4627
Jonas Grau <jonas.grau@gmail.com>
2018-11-11 23:55:46 +0100

    WIP bootstrap file

---
+++ script/bootstrap.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt --yes install \
+  zsh \
+  vim \
+  git \
+  tig \
+  xclip \
+  software-properties-common \
+  tmux \
+  direnv \
+  golang-go
+
+# Change the shell to zsh
+if [ "$(getent passwd "$LOGNAME" | cut -d: -f7)" != "/bin/zsh" ]; then
+  chsh -s /bin/zsh jgrau
+fi
+
+if [ ! -f "$HOME"/.ssh/id_rsa ]; then
+  # Create ssh keypair
+  ssh-keygen -t rsa -b 4096 -C "jonas.grau@gmail.com"
+
+  # Add to ssh-agent
+  eval "$(ssh-agent -s)"
+  ssh-add ~/.ssh/id_rsa
+
+  # Add public key to clipboard
+  xclip -sel clip <~/.ssh/id_rsa.pub
+
+  echo "Now go add you new public key to github - it has been copied to clipboard"
+  read -p "Press any key to continue or CTRL+C to quit" -r
+fi
+
+if [ ! -d "$HOME"/.dotfiles ]; then
+  # Clone the dotfiles
+  git clone git@github.com:jgrau/dotfiles.git ~/.dotfiles
+
+  sudo add-apt-repository ppa:martin-frost/thoughtbot-rcm
+  sudo apt-get update
+  sudo apt-get install rcm
+
+  rcup
+fi
+
+if [ ! -f /etc/modprobe.d/hid_apple.conf ]; then
+  # Swap ALT and CMD on apple keyboards
+  read -p "Swap <SUPER> and <ALT> keys (eg. on macbook keyboard) (requires restart to take effect) [y/n]" -n 1 -r
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
+    echo options hid_apple swap_opt_cmd=1 | sudo tee -a /etc/modprobe.d/hid_apple.conf
+    sudo update-initramfs -u
+  fi
+fi
+
+# Install shfmt
+go get -u mvdan.cc/sh/cmd/shfmt

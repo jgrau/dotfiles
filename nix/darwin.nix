4f0fe1b031055a3eeb63d8b1e70b7019f6fec5a0
Jonas Grau <jonas.grau@gmail.com>
2026-07-21 16:13:02 +0200

    Add Handy speech-to-text cask

--- nix/darwin.nix
+++ nix/darwin.nix
@@ -59,6 +59,7 @@
       "1password"
       "brave-browser"
       "google-chrome"
+      "handy"       # local speech-to-text app (https://handy.computer)
       "iina"
       "lens"
       "linear"
